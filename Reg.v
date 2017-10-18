812f845194cd48ed8d98534a0ea221a4a7fdcaaa
h@di <hadisafari76@gmail.com>
2017-10-18 12:00:50 +0330

    reformat

--- Reg.v
+++ Reg.v
@@ -1,4 +1,7 @@
-module Reg #(parameter n = 1) (input clk, rst, ld, input  [n - 1:0] parin, output reg  [n - 1:0] w);
+module Reg #(parameter n = 1) (
+    input clk, rst, ld, input  [n - 1:0] parin,
+    output reg  [n - 1:0] w
+);
   always@(posedge clk) begin
     if(rst == 1)
       w <= 0;
